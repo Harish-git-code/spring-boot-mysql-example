bac12dd3b90e8abe10f1db1bf89a137118f93467
Harish <125264442+Harish-git-code@users.noreply.github.com>
2024-01-31 17:04:55 +0530

    Update pre-requisite.sh

--- pre-requisite.sh
+++ pre-requisite.sh
@@ -8,13 +8,13 @@ echo 'ubuntu' | sudo -S apt-get update
 echo 'ubuntu' | sudo -S apt-get install mysql-server
 echo 'ubuntu' | sudo -S systemctl start mysql.service
 echo 'ubuntu' | sudo -S apt-get install mysql-client
-echo 'ubuntu' | sudo -S mysql
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password123';
-exit
-echo 'ubuntu' | sudo -S mysql -uroot -ppassword123
-CREATE DATABASE springboot_mysql_example;
-SHOW DATABASES;
-exit
+#echo 'ubuntu' | sudo -S mysql
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password123';
+#exit
+#echo 'ubuntu' | sudo -S mysql -uroot -ppassword123
+#CREATE DATABASE springboot_mysql_example;
+#SHOW DATABASES;
+#exit
 cd spring-boot-mysql-example/src/main/resources/
 sed -i 's/spring.datasource.url= jdbc:mysql://localhost:3306/springboot_mysql_example/spring.datasource.url= jdbc:mysql://localhost:3306/springboot_mysql_example?useSSL=false/' application.properties
 sed -i 's/spring.datasource.username= root/spring.datasource.username= root' application.properties
